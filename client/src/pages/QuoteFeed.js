75a06ccf9169a4e37e9b6641e59d14215f389395
Shahidullah <shahidullah.rahman223@gmail.com>
2019-11-26 20:05:14 -0500

    Added fetch function and render on client side

---
+++ client/src/pages/QuoteFeed.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import Quotes from "../components/Quote";
+
+export default class QuoteFeed extends Component {
+  state = {
+    quotes: "",
+    author: "",
+    loading: true
+  };
+
+  componentDidMount() {
+    fetch("http://quotes.rest/qod.json?category=inspire")
+      .then(response => response.json())
+      .then(data => {
+        let quote = data.contents.quotes[0].quote;
+        let author = data.contents.quotes[0].author;
+        console.log(quote);
+        this.setState({ quotes: quote, author: author });
+      });
+  }
+
+  render() {
+    console.log(this.state.quotes);
+    return (
+      <div>
+        <Quotes quote={this.state.quotes} author={this.state.author} />
+      </div>
+    );
+  }
+}
