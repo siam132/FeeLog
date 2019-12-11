ba0bd592f4d22a43c1e193f6042ea2a26c688b14
reudyreud <dreudverly@yahoo.com>
2019-12-11 11:22:57 -0500

    minor tweaks in api call

--- api/services/watson.js
+++ api/services/watson.js
@@ -1,4 +1,3 @@
-//------------------------------------------------------------------------------
 const ToneAnalyzerV3 = require("ibm-watson/tone-analyzer/v3");
 const { IamAuthenticator } = require("ibm-watson/auth");
 
@@ -13,10 +12,6 @@ const toneAnalyzer = new ToneAnalyzerV3({
 
 
 function analyzeTone(contentText) {
-    // const text =
-    // "I'm selfish, impatient and a little insecure. I make mistakes," + 
-    // " I am out of control and at times hard to handle. But if you can't handle me at my worst,"
-    // + "then you sure as hell don't deserve me at my best.";
     const toneParams = {
         toneInput: { text: contentText },
         contentType: "application/json"
@@ -29,7 +24,7 @@ function analyzeTone(contentText) {
             let tones = [];
             for(var i = 0; i < emotions.length; i++){
                 var data =  JSON.stringify(emotions[i].tone_name,null,2);
-                console.log(data);
+                // console.log(data);
                 tones.push(data);
             }
             return tones;
@@ -40,5 +35,3 @@ function analyzeTone(contentText) {
 }
   
 module.exports = { analyzeTone };
-
-//------------------------------------------------------------------------------
