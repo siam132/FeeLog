7d0404d0d3a749cf0228757d1912bb1e28d2f9a8
Carol Pignato <carolpignato97@gmail.com>
2019-12-11 23:33:02 -0500

    fixed syntax

--- client/src/assets/Quote_file.js
+++ client/src/assets/Quote_file.js
@@ -1,51 +1,52 @@
-let Quote_json =  [
-    {
-        "quote" : "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”",
-        "author" : "Merlin Menroe"
-        
-    }, 
+let Quote_json = [
+  {
+    quote:
+      "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”",
+    author: "Merlin Menroe"
+  },
 
-    {
-        "quote": "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”",
-        "author" : "Merlin Menroe"
-    },
-    {
-        "quote": "Be yourself; everyone else is already taken.",
-        "author": "Oscar Wilde"
-    },
-    {
-        "quote": "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
-        "author": "Bernard M. Baruch"
-    },
-    {
-        "quote": "You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.",
-        "author": "William W. Purkey"
-    },
+  {
+    quote:
+      "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”",
+    author: "Merlin Menroe"
+  },
+  {
+    quote: "Be yourself; everyone else is already taken.",
+    author: "Oscar Wilde"
+  },
+  {
+    quote:
+      "Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.",
+    author: "Bernard M. Baruch"
+  },
+  {
+    quote:
+      "You've gotta dance like there's nobody watching, Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.",
+    author: "William W. Purkey"
+  },
+  {
+    quote: "Sustenance! Your health is always the best prescription.",
+    author: "Lorii Myers"
+  },
+  {
+    quote:
+      "You must have a positive self perception in order to transcend anything.",
+    author: "Steve Maraboli"
+  },
+  {
+    quote:
+      "If you’re changing the world, you’re working on important things. You’re excited to get up in the morning. ",
+    author: "Larry Page"
+  },
+  {
+    quote:
+      "If you don't give up, you still have a chance. And when you are small, you have to be very focused and rely on your brain, not your strength.",
+    author: "Jack Ma"
+  },
+  {
+    quote: "Hang out with people who are better than you.",
+    author: "Warren Buffett"
+  }
+];
 
-    {
-        "quote": "Sustenance! Your health is always the best prescription."
-        "author": "Lorii Myers"
-    },
-
-    {
-        "quote": "You must have a positive self perception in order to transcend anything."
-        "author": "Steve Maraboli"
-    },
-
-    {
-        "quote" : "If you’re changing the world, you’re working on important things. You’re excited to get up in the morning. "
-        "author" : "Larry Page"
-    },
-
-    {
-        "quote" : "If you don't give up, you still have a chance. And when you are small, you have to be very focused and rely on your brain, not your strength."
-        "author" : "Jack Ma"
-    },
-
-    {
-        "quote" : "Hang out with people who are better than you."
-        "author" : "Warren Buffett"
-     }
-]
-
-module.exports = Quote_json; 
+module.exports = Quote_json;
