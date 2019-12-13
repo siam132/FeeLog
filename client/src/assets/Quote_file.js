a3a3c624e369c15fff395c4909520a483b61ad01
Carol Pignato <carolpignato97@gmail.com>
2019-12-12 19:23:46 -0500

    added quotes to database

--- client/src/assets/Quote_file.js
+++ client/src/assets/Quote_file.js
@@ -1,9 +1,8 @@
-
-let Quote_json =  [
+let Quote_json = [
   {
     quote:
       "“I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.”",
-    author: "Merlin Menroe"
+    author: "Marilyn Monroe"
   },
   {
     quote: "Be yourself; everyone else is already taken.",
@@ -43,29 +42,106 @@ let Quote_json =  [
     author: "Warren Buffett"
   },
   {
-    quote: "As we express our gratitude, we must never forget that the highest appreciation is not to utter words, but to live by them.",
+    quote:
+      "As we express our gratitude, we must never forget that the highest appreciation is not to utter words, but to live by them.",
     author: "John F. Kennedy"
-  }
-  ,
+  },
   {
-    quote: "It is during our darkest moments that we must focus to see the light.",
+    quote:
+      "It is during our darkest moments that we must focus to see the light.",
     author: "Aristotle"
   },
   {
-    quote: "You will never do anything in this world without courage. It is the greatest quality of the mind next to honor.",
+    quote:
+      "You will never do anything in this world without courage. It is the greatest quality of the mind next to honor.",
     author: "Aristotle"
   },
   {
-    quote: "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear.",
+    quote:
+      "I learned that courage was not the absence of fear, but the triumph over it. The brave man is not he who does not feel afraid, but he who conquers that fear.",
     author: "Nelson Mandela"
   },
   {
-    quote: "Two roads diverged in a wood and I - I took the one less traveled by, and that has made all the difference",
+    quote:
+      "Two roads diverged in a wood and I - I took the one less traveled by, and that has made all the difference",
     author: "Robert Frost"
   },
   {
-    quote: "Education is the ability to listen to almost anything without losing your temper or your self-confidence.",
+    quote:
+      "Education is the ability to listen to almost anything without losing your temper or your self-confidence.",
     author: "Robert Frost"
+  },
+  {
+    quote: "What we fear doing most is usually what we most need to do.",
+    author: "Ralph Waldo Emerson"
+  },
+  {
+    quote:
+      "You are essentially who you create yourself to be and all that occurs in your life is the result of your own making",
+    author: "Stephen Richards"
+  },
+  {
+    quote:
+      "Of course motivation is not permanent. But then, neither is bathing; but it is something you should do on a regular basis.",
+    author: "Zig Ziglar"
+  },
+  {
+    quote:
+      "There is only one thing that makes a dream impossible to achieve: the fear of failure.",
+    author: "Paulo Coelho"
+  },
+  {
+    quote:
+      "If you are insecure, guess what? The rest of the world is, too. Do not overestimate the competition and underestimate yourself. You are better than you think.",
+    author: "Timothy Ferriss"
+  },
+  {
+    quote:
+      "It isn't what you have or who you are or where you are or what you are doing that makes you happy or unhappy. It is what you think about it.",
+    author: "Dale Carnegie"
+  },
+  {
+    quote:
+      "Those who believe they can move mountains, do. Those who believe they can't, cannot. Belief triggers the power to do.",
+    author: "David J. Schwartz"
+  },
+  {
+    quote:
+      "It is only because of problems that we grow mentally and spiritually.",
+    author: "M. Scott Peck"
+  },
+  {
+    quote:
+      "Until you value yourself, you won't value your time. Until you value your time, you will not do anything with it.",
+    author: "M. Scott Peck"
+  },
+  {
+    quote:
+      "Love isn’t a state of perfect caring. It is an active noun like struggle. To love someone is to strive to accept that person exactly the way he or she is, right here and now.",
+    author: "Fred Rogers"
+  },
+  {
+    quote: "Self-love, my liege, is not so vile a sin, as self-neglecting.",
+    author: "William Shakespeare"
+  },
+  {
+    quote:
+      "Love comes when manipulation stops; when you think more about the other person than about his or her reactions to you. When you dare to reveal yourself fully. When you dare to be vulnerable.",
+    author: "Dr. Joyce Brothers"
+  },
+  {
+    quote:
+      "Loving yourself…does not mean being self-absorbed or narcissistic, or disregarding others. Rather it means welcoming yourself as the most honored guest in your own heart, a guest worthy of respect, a lovable companion.",
+    author: "Margo Anand"
+  },
+  {
+    quote: "The only time you fail is when you fall down and stay down.",
+    author: "Stephen Richards"
+  },
+  {
+    quote:
+      "Learn to deal with the fact that you are not a perfect person but you are a person that deserves respect and honesty.",
+    author: "Pandora Poikilos"
   }
 ];
 
