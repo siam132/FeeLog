7428e00bd3c920af6022e17e30e5cc7e5e377051
Shahidullah <shahidullah.rahman223@gmail.com>
2019-12-13 10:14:53 -0500

    Added better randomizing algo for quote feed

--- client/src/pages/QuoteFeed.js
+++ client/src/pages/QuoteFeed.js
@@ -29,9 +29,9 @@ export default class QuoteFeed extends Component {
 
         <div className="quote-feed">
           {Quote_json.reverse().map(e => {
-            const prob = Math.random(5) * 10;
+            const prob = Math.floor(Math.random() * (5 - 0)) + 0;
 
-            if (prob < 2) return <Quotes quote={e.quote} author={e.author} />;
+            if (prob < 1) return <Quotes quote={e.quote} author={e.author} />;
           })}
           <Quotes quote={this.state.quotes} author={this.state.author} />
         </div>
